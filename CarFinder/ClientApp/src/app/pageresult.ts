2d82b30c77cf4040fe3f8445a7eb84a27d2d0760
Benard Charumbira <benard@yobi.co.za>
2021-04-28 17:47:47 +0200

    cleanup

--- CarFinder/ClientApp/src/app/pageresult.ts
+++
@@ -1,8 +0,0 @@
-export class PageResult {
-    constructor(
-        public count: number,
-        public pageIndex: number,
-        public pageSize: number,
-        public items: any[]
-     ) {}
-   }
